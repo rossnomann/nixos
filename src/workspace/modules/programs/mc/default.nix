391ee77726e5491c75cfb07e696b63f196d2ccca
Ross Nomann <rossnomann@protonmail.com>
2024-04-19 23:31:07 +0200

    Use xdg-open in mc for images and some documents

--- src/workspace/modules/programs/mc/default.nix
+++ src/workspace/modules/programs/mc/default.nix
@@ -3,6 +3,7 @@
   environment.systemPackages = [ pkgs.mc ];
   home-manager.users.${config.workspace.user.name}.xdg = {
     configFile."mc/ini".source = ./resources/config.ini;
+    configFile."mc/mc.ext.ini".source = ./resources/mc.ext.ini;
     dataFile."mc/skins/catppuccin.ini".source = ./resources/catppuccin.ini;
   };
 }
