17dd6e5730bc2d28c0423d89ae2b769b240d3ab6
Ross Nomann <rossnomann@protonmail.com>
2024-04-24 21:27:01 +0200

    Setup mime types for transmission

--- src/workspace/modules/programs/transmission.nix
+++ src/workspace/modules/programs/transmission.nix
@@ -1,4 +1,31 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.transmission-gtk ];
+  home-manager.users.${config.workspace.user.name} = {
+    xdg = {
+      dataFile = {
+        "applications/transmission.dekstop".text = ''
+          [Desktop Entry]
+          Encoding=UTF-8
+          Version=1.0
+          Type=Application
+          NoDisplay=true
+          Exec=transmission-gtk %u
+          Name=transmission-gtk
+          Comment=Custom definition for transmission-gtk
+        '';
+      };
+      mimeApps =
+        let
+          assoc = {
+            "x-scheme-handler/magnet" = [ "transmission.desktop" ];
+            "application/x-bittorrent" = [ "transmission.desktop" ];
+          };
+        in
+        {
+          associations.added = assoc;
+          defaultApplications = assoc;
+        };
+    };
+  };
 }
