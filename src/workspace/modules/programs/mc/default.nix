51be49aac81f8c6ee26e06671cc4ed61a9f71028
Ross Nomann <rossnomann@protonmail.com>
2024-04-27 20:59:22 +0200

    Configure default association for inode/directory

--- src/workspace/modules/programs/mc/default.nix
+++ src/workspace/modules/programs/mc/default.nix
@@ -4,6 +4,14 @@
   home-manager.users.${config.workspace.user.name}.xdg = {
     configFile."mc/ini".source = ./resources/config.ini;
     configFile."mc/mc.ext.ini".source = ./resources/mc.ext.ini;
+    dataFile."applications/mc.desktop".source = ./resources/mc.desktop;
     dataFile."mc/skins/catppuccin.ini".source = ./resources/catppuccin.ini;
+    mimeApps.defaultApplications =
+        let
+          defaults = [ "mc.desktop" ];
+        in
+        {
+          "inode/directory" = defaults;
+        };
   };
 }
