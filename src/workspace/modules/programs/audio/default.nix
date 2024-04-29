edda03379f021c887a342af63f8960b4e8116318
Ross Nomann <rossnomann@protonmail.com>
2024-04-30 00:30:48 +0200

    Replace audacity with oceanaudio

--- src/workspace/modules/programs/audio/default.nix
+++ src/workspace/modules/programs/audio/default.nix
@@ -3,12 +3,12 @@
   home-manager.users.${config.workspace.user.name} = {
     home.packages = [
       pkgs.ardour
-      pkgs.audacity
       pkgs.cplay-ng
       pkgs.deadbeef
       pkgs.guitarix
       pkgs.gxplugins-lv2
       pkgs.hydrogen
+      pkgs.ocenaudio
       pkgs.tuxguitar
     ];
     xdg.dataFile."applications/tuxguitar.desktop".source = ./resources/tuxguitar.desktop;
