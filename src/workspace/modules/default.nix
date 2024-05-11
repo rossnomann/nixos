78fabdcc254a9651ff810750f943f794ee787d0f
Ross Nomann <rossnomann@protonmail.com>
2024-05-12 00:35:29 +0200

    Merge python module into env, rename docker module to vt

--- src/workspace/modules/default.nix
+++ src/workspace/modules/default.nix
@@ -1,16 +1,15 @@
 { ... }:
 {
   imports = [
-    ./docker.nix
     ./env.nix
     ./net.nix
     ./nixos.nix
     ./programs
-    ./python
     ./security.nix
     ./services.nix
     ./ui
     ./users.nix
     ./utils.nix
+    ./vt.nix
   ];
 }
--- src/workspace/modules/env.nix
+++ src/workspace/modules/env.nix
@@ -30,6 +30,8 @@
       PGSERVICEFILE = "$XDG_CONFIG_HOME/psql/service.conf";
       PSQL_HISTORY = "$XDG_DATA_HOME/psql/history";
       PSQLRC = "$XDG_CONFIG_HOME/psql/config";
+      PYTHONSTARTUP = "$XDG_CONFIG_HOME/python/pythonrc";
+      PYTHON_HISTORY = "$XDG_DATA_HOME/python/history";
       REDISCLI_HISTFILE = "$XDG_DATA_HOME/redis/history";
       REDISCLI_RCFILE = "$XDG_CONFIG_HOME/redis/config";
       SQLITE_HISTORY = "$XDG_DATA_HOME/sqlite/history";
@@ -42,7 +44,48 @@
       enable = true;
       mimeApps.enable = true;
 
-      configFile."mimeapps.list".force = true;
+      configFile = {
+        "mimeapps.list".force = true;
+        "python/pythonrc" = {
+          executable = true;
+          text = ''
+            #!/usr/bin/env python
+
+
+            def __setup_history():
+                import atexit
+                import os
+                import readline
+                import sys
+
+                from contextlib import suppress
+                from pathlib import Path
+
+                if hasattr(sys, "__interactivehook__"):
+                    del sys.__interactivehook__
+
+                if data_home := os.environ.get("XDG_DATA_HOME"):
+                    data_home = Path(data_home)
+                else:
+                    data_home = Path.home() / ".local" / "share"
+                if not data_home.is_dir():
+                    print("Error: XDG_DATA_HOME does not exist at", data_home)
+
+                history_path = data_home / "python" / "history"
+                if not history_path.parent.is_dir():
+                    history_path.parent.mkdir()
+                history_path.touch(exist_ok=True)
+
+                with suppress(OSError):
+                    readline.read_history_file(history_path)
+                readline.set_history_length(5000)
+                atexit.register(readline.write_history_file, history_path)
+
+
+            __setup_history()
+          '';
+        };
+      };
       userDirs =
         let
           homeDirectory = config.home-manager.users.${config.workspace.user.name}.home.homeDirectory;
