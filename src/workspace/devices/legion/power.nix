31ded7e96433be9acf51efaca9e1e31323369f10
Ross Nomann <rossnomann@protonmail.com>
2024-05-12 23:46:39 +0200

    Update logind settings on legion

--- src/workspace/devices/legion/power.nix
+++ src/workspace/devices/legion/power.nix
@@ -2,7 +2,17 @@
 {
   environment.systemPackages = [ pkgs.powertop ];
   powerManagement.powertop.enable = true;
-  services.logind.lidSwitch = "ignore";
+  services.logind = {
+    extraConfig = ''
+    IdleAction=ignore
+    '';
+    killUserProcesses = true;
+    lidSwitch = "ignore";
+    lidSwitchDocked = "ignore";
+    lidSwitchExternalPower = "ignore";
+    powerKey = "ignore";
+    powerKeyLongPress = "poweroff";
+  };
   systemd.sleep.extraConfig = ''
     AllowSuspend=no
     AllowHibernation=no
