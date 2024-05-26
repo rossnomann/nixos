ee6c58a39a3d2d64bacee36a506df22bc981d493
Ross Nomann <rossnomann@protonmail.com>
2024-05-26 10:17:14 +0200

    Install and configure some lv2/vst plugins

--- src/workspace/programs/audio.nix
+++ src/workspace/programs/audio.nix
@@ -1,18 +1,35 @@
 { config, pkgs, ... }:
 {
+  environment.sessionVariables.LV2_PATH = "$HOME/.nix-profile/lib/lv2";
   home-manager.users.${config.workspace.user.name} = {
     home.packages = [
+      # composing/recording
       pkgs.ardour
-      pkgs.cplay-ng
-      pkgs.deadbeef
       pkgs.easyeffects
       pkgs.guitarix
-      pkgs.gxplugins-lv2
-      pkgs.helvum
       pkgs.hydrogen
       pkgs.musescore
-      pkgs.pavucontrol
       pkgs.ocenaudio
+
+      # playing
+      pkgs.cplay-ng
+      pkgs.deadbeef
+
+      # settings
+      pkgs.helvum
+      pkgs.pavucontrol
+
+      # plugins
+      pkgs.calf
+      pkgs.distrho
+      pkgs.eq10q
+      pkgs.gxplugins-lv2
+      pkgs.lsp-plugins
+      pkgs.noise-repellent
+      pkgs.oxefmsynth
+      pkgs.tap-plugins
+      pkgs.x42-avldrums
+      pkgs.x42-plugins
     ];
   };
 }
