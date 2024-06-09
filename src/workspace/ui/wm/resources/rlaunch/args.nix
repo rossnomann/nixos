0cf256f698fb984913b1e2e57c28686a7ffb2718
Ross Nomann <rossnomann@protonmail.com>
2024-06-09 10:44:39 +0200

    fix rlaunch wrapper

--- src/workspace/ui/wm/resources/rlaunch/args.nix
+++ src/workspace/ui/wm/resources/rlaunch/args.nix
@@ -4,11 +4,11 @@ let
   palette = ui.palette;
 in
 [
-  "-f '${font.family}:size=${builtins.toString (font.defaultSize - 2)}'"
-  "--color0 '${palette.surface0}'"
-  "--color1 '${palette.surface1}'"
-  "--color2 '${palette.text}'"
-  "--color3 '${palette.text}'"
-  "--color4 '${palette.surface0}'"
+  "-f ${font.family}:size=${builtins.toString (font.defaultSize - 2)}"
+  "--color0 ${palette.surface0}"
+  "--color1 ${palette.surface1}"
+  "--color2 ${palette.text}"
+  "--color3 ${palette.text}"
+  "--color4 ${palette.surface0}"
   "--terminal alacritty"
 ]
