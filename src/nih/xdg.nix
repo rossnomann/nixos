28623d8305de0648c0be6137db8edbeceb7fbea6
Ross Nomann <rossnomann@protonmail.com>
2024-08-29 07:26:47 +0200

    Disable home-manager.xdg

--- src/nih/xdg.nix
+++ src/nih/xdg.nix
@@ -166,49 +166,46 @@ in
         addedAssociations = assoc;
         defaultApplications = assoc;
       };
-    home-manager.users.${cfgUser.name} = {
-      home.file = {
-        ".config/python/pythonrc" = {
-          executable = true;
-          text = ''
-            #!/usr/bin/env python
+    home-manager.users.${cfgUser.name}.home.file = {
+      ".config/python/pythonrc" = {
+        executable = true;
+        text = ''
+          #!/usr/bin/env python
 
 
-            def __setup_history():
-                import atexit
-                import os
-                import readline
-                import sys
+          def __setup_history():
+              import atexit
+              import os
+              import readline
+              import sys
 
-                from contextlib import suppress
-                from pathlib import Path
+              from contextlib import suppress
+              from pathlib import Path
 
-                if hasattr(sys, "__interactivehook__"):
-                    del sys.__interactivehook__
+              if hasattr(sys, "__interactivehook__"):
+                  del sys.__interactivehook__
 
-                if data_home := os.environ.get("XDG_DATA_HOME"):
-                    data_home = Path(data_home)
-                else:
-                    data_home = Path.home() / ".local" / "share"
-                if not data_home.is_dir():
-                    print("Error: XDG_DATA_HOME does not exist at", data_home)
+              if data_home := os.environ.get("XDG_DATA_HOME"):
+                  data_home = Path(data_home)
+              else:
+                  data_home = Path.home() / ".local" / "share"
+              if not data_home.is_dir():
+                  print("Error: XDG_DATA_HOME does not exist at", data_home)
 
-                history_path = data_home / "python" / "history"
-                if not history_path.parent.is_dir():
-                    history_path.parent.mkdir()
-                history_path.touch(exist_ok=True)
+              history_path = data_home / "python" / "history"
+              if not history_path.parent.is_dir():
+                  history_path.parent.mkdir()
+              history_path.touch(exist_ok=True)
 
-                with suppress(OSError):
-                    readline.read_history_file(history_path)
-                readline.set_history_length(5000)
-                atexit.register(readline.write_history_file, history_path)
+              with suppress(OSError):
+                  readline.read_history_file(history_path)
+              readline.set_history_length(5000)
+              atexit.register(readline.write_history_file, history_path)
 
 
-            __setup_history()
-          '';
-        };
+          __setup_history()
+        '';
       };
-      xdg.enable = true;
     };
   };
 }
