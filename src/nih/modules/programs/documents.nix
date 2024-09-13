11825999b2a46d3bf59dc64eeaeffc9528952ce6
Ross Nomann <rossnomann@protonmail.com>
2024-09-13 02:25:35 +0200

    Flatten top-level options in documents module

--- src/nih/modules/programs/documents.nix
+++ src/nih/modules/programs/documents.nix
@@ -17,31 +17,29 @@ in
       pkgs.simple-scan
       pkgs.zathura
     ];
-    nih = {
-      user.home.file = {
-        ".config/zathura/zathurarc".text = ''
-          include ${npins.catppuccin-zathura}/src/catppuccin-${cfgStyle.palette.variant}
-        '';
-      };
-      x11.wm.windowRules = [
-        {
-          windowClass = "libreoffice";
-          spawnOnTag = "documents";
-        }
-        {
-          windowClass = "obsidian";
-          spawnOnTag = "documents";
-        }
-        {
-          windowClass = "org.pwmt.zathura";
-          spawnOnTag = "documents";
-        }
-        {
-          windowClass = "simple-scan";
-          spawnOnTag = "documents";
-        }
-      ];
-      xdg.mime.documents = "org.pwmt.zathura.desktop";
+    nih.user.home.file = {
+      ".config/zathura/zathurarc".text = ''
+        include ${npins.catppuccin-zathura}/src/catppuccin-${cfgStyle.palette.variant}
+      '';
     };
+    nih.x11.wm.windowRules = [
+      {
+        windowClass = "libreoffice";
+        spawnOnTag = "documents";
+      }
+      {
+        windowClass = "obsidian";
+        spawnOnTag = "documents";
+      }
+      {
+        windowClass = "org.pwmt.zathura";
+        spawnOnTag = "documents";
+      }
+      {
+        windowClass = "simple-scan";
+        spawnOnTag = "documents";
+      }
+    ];
+    xdg.mime.documents = "org.pwmt.zathura.desktop";
   };
 }
