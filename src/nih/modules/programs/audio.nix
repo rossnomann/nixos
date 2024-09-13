07d696c2c3aed601bde5fea3589b151eca09d5bd
Ross Nomann <rossnomann@protonmail.com>
2024-09-13 02:16:39 +0200

    Update audio module
    - Run deadbeef on audio tag
    - Remove plugins from systemPackages; they are still available via environment variables
    - Add window rule for easyeffects

--- src/nih/modules/programs/audio.nix
+++ src/nih/modules/programs/audio.nix
@@ -6,64 +6,60 @@
 }:
 let
   cfg = config.nih;
-  makeLv2Path = pkg: "${pkg}/lib/lv2";
-  makeLadspaPath = pkg: "${pkg}/lib/ladspa";
-  pluginsLv2 = [
-    pkgs.calf
-    pkgs.distrho
-    pkgs.eq10q
-    pkgs.gxplugins-lv2
-    pkgs.guitarix
-    pkgs.kapitonov-plugins-pack
-    pkgs.lsp-plugins
-    pkgs.neural-amp-modeler-lv2
-    pkgs.noise-repellent
-    pkgs.rkrlv2
-    pkgs.x42-avldrums
-    pkgs.x42-plugins
-  ];
-  pluginsLadspa = [
-    pkgs.caps
-    pkgs.tap-plugins
-  ];
-  plugins = pluginsLv2 ++ pluginsLadspa;
 in
 {
   config = lib.mkIf cfg.enable {
-    environment = {
-      sessionVariables = {
+    environment.sessionVariables =
+      let
+        makeLv2Path = pkg: "${pkg}/lib/lv2";
+        makeLadspaPath = pkg: "${pkg}/lib/ladspa";
+        pluginsLv2 = [
+          pkgs.calf
+          pkgs.distrho
+          pkgs.eq10q
+          pkgs.gxplugins-lv2
+          pkgs.guitarix
+          pkgs.kapitonov-plugins-pack
+          pkgs.lsp-plugins
+          pkgs.neural-amp-modeler-lv2
+          pkgs.noise-repellent
+          pkgs.rkrlv2
+          pkgs.x42-avldrums
+          pkgs.x42-plugins
+        ];
+        pluginsLadspa = [
+          pkgs.caps
+          pkgs.tap-plugins
+        ];
+      in
+      {
         LADSPA_PATH = lib.concatStringsSep ":" (map makeLadspaPath pluginsLadspa);
         LV2_PATH = lib.concatStringsSep ":" (map makeLv2Path pluginsLv2);
       };
-      systemPackages = [
-        # composing/recording
-        pkgs.ardour
-        pkgs.easyeffects
-
-        pkgs.hydrogen
-
-        # encoding
-        pkgs.lame
-
-        # playing
-        pkgs.deadbeef
-
-        # settings
-        pkgs.helvum
-        pkgs.pavucontrol
-      ] ++ plugins;
-    };
+    environment.systemPackages = [
+      pkgs.ardour
+      pkgs.deadbeef
+      pkgs.easyeffects
+      pkgs.helvum
+      pkgs.hydrogen
+      pkgs.lame
+      pkgs.pavucontrol
+    ];
     nih.x11.wm.windowRules = [
-      {
-        windowClass = "deadbeef";
-        spawnOnTag = "secondary";
-      }
       {
         windowClass = "ardour-8.4.0";
         spawnOnTag = "audio";
       }
       {
         windowClass = "ardour_ardour";
+        spawnOnTag = "audio";
+      }
+      {
+        windowClass = "deadbeef";
+        spawnOnTag = "audio";
+      }
+      {
+        windowClass = "easyeffects";
         spawnOnTag = "audio";
       }
       {
