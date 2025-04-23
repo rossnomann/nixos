6bbad5cbb5fc0449f6a7101628455fa7905194f0
Ross Nomann <rossnomann@protonmail.com>
2025-04-23 20:36:45 +0200

    tlp: set START_/STOP_CHARGE_THRESH_BAT0

--- src/nih/modules/power.nix
+++ src/nih/modules/power.nix
@@ -41,6 +41,8 @@ in
         CPU_SCALING_GOVERNOR_ON_BAT = "schedutil";
         CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
         CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
+        START_CHARGE_THRESH_BAT0 = 45;
+        STOP_CHARGE_TRESH_BAT0 = 50;
       };
     };
     systemd.sleep.extraConfig =
