7867c9d49ffe78f0aff0149091551454c20b13b0
Ross Nomann <rossnomann@protonmail.com>
2025-05-03 12:11:29 +0200

    Extra locales all is no longer supported

--- src/nih/modules/locale.nix
+++ src/nih/modules/locale.nix
@@ -22,7 +22,7 @@ in
       LC_TELEPHONE = cfgLocale.extra;
       LC_TIME = cfgLocale.extra;
     };
-    i18n.extraLocales = [ "all" ];
+    i18n.extraLocales = [ "en_US.UTF-8/UTF-8" "ru_RU.UTF-8/UTF-8" ];
     time.timeZone = cfgLocale.timeZone;
   };
 }
