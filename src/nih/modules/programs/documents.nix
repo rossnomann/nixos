a1b542439bdcbd4eb32a4ab31c43fe485b930d87
Ross Nomann <rossnomann@protonmail.com>
2025-09-24 06:05:12 +0200

    Make obsidian always open vault manager on startup

--- src/nih/modules/programs/documents.nix
+++ src/nih/modules/programs/documents.nix
@@ -11,12 +11,39 @@ let
 in
 {
   config = lib.mkIf cfg.enable {
-    environment.systemPackages = [
-      pkgs.libreoffice
-      pkgs.obsidian
-      pkgs.simple-scan
-      pkgs.zathura
-    ];
+    environment.systemPackages =
+      let
+        obsidianHook = pkgs.writeTextFile {
+          name = "obsidian-hook";
+          text = ''
+            #!${pkgs.python313}/bin/python
+            import json
+            from pathlib import Path
+
+            PATH = Path("~/.config/obsidian/obsidian.json").expanduser()
+            if PATH.exists():
+                with PATH.open() as f:
+                    data = json.load(f)
+                    if isinstance(data, dict) and "vaults" in data:
+                        vaults = data["vaults"]
+                        for i in vaults.values():
+                            i.pop("open", None)
+                with PATH.open("w") as f:
+                    json.dump(data, f)
+          '';
+          executable = true;
+        };
+      in
+      [
+        pkgs.libreoffice
+        (pkgs.obsidian.overrideAttrs (x: {
+          postInstall = ''
+            sed -i '1 a ${obsidianHook}' $out/bin/obsidian
+          '';
+        }))
+        pkgs.simple-scan
+        pkgs.zathura
+      ];
     nih.user.home.file = {
       ".config/zathura/zathurarc".text = ''
         include ${cfgSources.catppuccin-zathura}/src/catppuccin-${cfgStyle.palette.variant}
