931e822cca273bba10c38b666e3106fa285862bd
Ross Nomann <rossnomann@protonmail.com>
2025-12-24 08:00:11 +0100

    Update login configuration
    - Set text greeter option for greetd
    - Update /etc/issue

--- src/nih/modules/login.nix
+++ src/nih/modules/login.nix
@@ -13,6 +13,10 @@ in
     command = lib.mkOption { type = lib.types.str; };
   };
   config = lib.mkIf cfg.enable {
+    services.getty = {
+      greetingLine = "\l ${config.system.nixos.label}";
+      helpLine = "";
+    };
     services.greetd =
       let
         tuigreet = "${pkgs.tuigreet}/bin/tuigreet";
@@ -20,6 +24,7 @@ in
       {
         enable = true;
         settings.default_session.command = "${tuigreet} --cmd ${cfgLogin.command}";
+        useTextGreeter = true;
       };
     systemd.user.units."wm-session.target" = {
       name = "wm-session.target";
