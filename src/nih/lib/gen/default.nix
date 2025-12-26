085c92f94f1140c59d9073e5b2bccb40958e63c6
Ross Nomann <rossnomann@protonmail.com>
2025-12-26 17:54:01 +0100

    Remove unused lib.nih.gen.leftwm attrset

--- src/nih/lib/gen/default.nix
+++ src/nih/lib/gen/default.nix
@@ -2,7 +2,6 @@ lib: {
   dunst = import ./dunst.nix lib;
   gtk = import ./gtk.nix;
   kdl = import ./kdl.nix lib;
-  leftwm = import ./leftwm.nix lib;
   macchina = import ./macchina.nix lib;
   mc = import ./mc.nix lib;
   xresources = import ./xresources.nix lib;
