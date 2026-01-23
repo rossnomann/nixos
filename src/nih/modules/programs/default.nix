f04cb3704a8d7292a2a261604a437a87452b0094
Ross Nomann <rossnomann@protonmail.com>
2026-01-23 20:55:11 +0100

    Merge programs/cli and programs/desktop, extract packages

--- src/nih/modules/programs/default.nix
+++ src/nih/modules/programs/default.nix
@@ -1,7 +1,184 @@
-{ ... }:
 {
-  imports = [
-    ./cli.nix
-    ./desktop.nix
-  ];
+  config,
+  fretboard,
+  lib,
+  pkgs,
+  pkgs20251111,
+  ...
+}:
+let
+  cfg = config.nih;
+  cfgSources = cfg.sources;
+  cfgStyle = cfg.style;
+  p = import ./packages.nix {
+    inherit
+      fretboard
+      pkgs
+      pkgs20251111
+      cfgSources
+      ;
+  };
+in
+{
+  options.nih.programs.desktop = {
+    alacritty.executable = lib.mkOption { type = lib.types.str; };
+    rofi.cmdShow = lib.mkOption { type = lib.types.listOf lib.types.str; };
+  };
+  config = lib.mkIf cfg.enable {
+    environment.systemPackages = lib.attrValues p;
+    environment.variables = {
+      MOZ_USE_XINPUT2 = "1";
+    };
+    programs.steam = {
+      enable = true;
+      gamescopeSession.enable = true;
+    };
+    xdg.mime =
+      let
+        assoc = {
+          "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
+        };
+      in
+      {
+        addedAssociations = assoc;
+        defaultApplications = assoc;
+      };
+    nih.graphicalSession.windowRules = [
+      {
+        appId = "^Alacritty";
+        workspace = "terminal";
+      }
+      {
+        appId = "^Ardour";
+        workspace = "audio";
+      }
+      {
+        appId = "^deadbeef";
+        workspace = "secondary";
+      }
+      {
+        appId = "^discord";
+        workspace = "secondary";
+      }
+      {
+        appId = "^firefox";
+        workspace = "main";
+      }
+      {
+        appId = "^gimp";
+        workspace = "main";
+      }
+      {
+        appId = ''^fr\\.greyc\\.$'';
+        workspace = "main";
+      }
+      {
+        appId = ''^org\\.pipewire\\.Helvum'';
+        workspace = "audio";
+      }
+      {
+        appId = ''^org\\.inkscape\\.Inkscape'';
+        workspace = "main";
+      }
+      {
+        appId = "^libreoffice";
+        workspace = "main";
+      }
+      {
+        appId = "^mpv";
+        workspace = "main";
+        fullscreen = true;
+      }
+      {
+        appId = "^obsidian";
+        workspace = "main";
+      }
+      {
+        appId = "^Slack";
+        workspace = "secondary";
+      }
+      {
+        appId = "^steam";
+        fullscreen = true;
+        workspace = "games";
+      }
+      {
+        appId = "^syncplay";
+        workspace = "main";
+        floating = true;
+      }
+      {
+        appId = ''^org\\.telegram\\.desktop'';
+        workspace = "secondary";
+      }
+      {
+        appId = "^transmission";
+        workspace = "main";
+      }
+      {
+        appId = ''^org\\.pwmt\\.zathura'';
+        workspace = "main";
+      }
+    ];
+    nih.programs.desktop = {
+      alacritty.executable = "${p.alacritty}/bin/alacritty";
+      rofi.cmdShow = [
+        "${p.rofi}/bin/rofi"
+        "-show"
+        "combi"
+      ];
+    };
+    nih.user.home.file =
+      let
+        palette = cfgStyle.palette;
+      in
+      {
+
+        ".config/alacritty/alacritty.toml".text = import ./resources/alacritty.nix {
+          fontMonospace = cfgStyle.fonts.monospace;
+          themePath = "${cfgSources.catppuccin-alacritty}/catppuccin-${cfgStyle.palette.variant}.toml";
+        };
+        ".config/ardour8/my-dark-ardour-8.12.colors".source = ./resources/ardour.colors;
+        ".config/fretboard/config.toml".text = import ./resources/fretboard.nix palette;
+        ".config/macchina/macchina.toml".source = ./resources/macchina-config.toml;
+        ".config/macchina/themes/default.toml".source = ./resources/macchina-theme.toml;
+        ".config/mc/ini".source = ./resources/mc.ini;
+        ".config/mc/mc.ext.ini".text =
+          let
+            xdgOpen = "${p.nohupXdgOpen}/bin/nohup-xdg-open";
+          in
+          lib.generators.toINI { } {
+            "mc.ext.ini" = {
+              Version = 4.0;
+            };
+            Default = {
+              Open = "${xdgOpen} %d/%p";
+              View = "${xdgOpen} %d/%p";
+            };
+          };
+        ".config/mpv/mpv.conf".text =
+          let
+            theme = builtins.readFile "${p.mpvCatppuccin}/${palette.variant}/${palette.accent}.conf";
+          in
+          ''
+            ${theme}
+          '';
+        ".config/rofi/config.rasi".text = import ./resources/rofi.nix cfgStyle.fonts.monospace.defaultSize;
+        ".config/rofi/theme.rasi".source = "${cfgSources.catppuccin-rofi}/catppuccin-default.rasi";
+        ".config/rofi/palette.rasi".source =
+          "${cfgSources.catppuccin-rofi}/themes/catppuccin-${cfgStyle.palette.variant}.rasi";
+        ".config/zathura/zathurarc".text = ''
+          include ${cfgSources.catppuccin-zathura}/themes/catppuccin-${cfgStyle.palette.variant}
+        '';
+        ".local/share/mc/skins/catppuccin.ini".source = "${cfgSources.catppuccin-mc}/catppuccin.ini";
+      };
+    nih.xdg.mime = {
+      archives = "xarchiver.desktop";
+      audio = "mpv.desktop";
+      documents = "org.pwmt.zathura.desktop";
+      images = "org.gnome.Loupe.desktop";
+      torrents = "transmission-gtk.desktop";
+      videos = "mpv.desktop";
+    };
+  };
 }
---
+++ src/nih/modules/programs/packages.nix
@@ -0,0 +1,83 @@
+{
+  fretboard,
+  pkgs,
+  pkgs20251111,
+  cfgSources,
+}:
+{
+  inherit (pkgs)
+    alacritty
+    ardour
+    discord
+    exiftool
+    file
+    helvum
+    htop
+    imagemagick
+    inkscape
+    lame
+    libreoffice
+    loupe
+    lshw
+    macchina
+    mc
+    mpv
+    overskride
+    pciutils
+    rofi
+    scrcpy
+    simple-scan
+    slack
+    syncplay
+    telegram-desktop
+    transmission_4-gtk
+    trash-cli
+    unrar
+    usbutils
+    wiremix
+    xarchiver
+    zathura
+    ;
+  deadbeef = pkgs20251111.deadbeef;
+  firefox =
+    let
+      extraPolicies = import ./resources/firefox-policies.nix;
+    in
+    pkgs.firefox.override { inherit extraPolicies; };
+  fretboard = fretboard.packages.${pkgs.stdenv.hostPlatform.system}.default;
+  gimp = pkgs.gimp3-with-plugins.override {
+    plugins = [ pkgs.gimp3Plugins.gmic ];
+  };
+  mpvCatppuccin =
+    let
+      src = cfgSources.catppuccin-mpv;
+    in
+    pkgs.stdenvNoCC.mkDerivation {
+      inherit src;
+      pname = "catppuccin-mpv";
+      version = src.revision;
+      installPhase = ''
+        runHook preInstall
+        mkdir -p $out
+        find themes/*.conf -type f -exec sed -i "s/^background-color=.*$/background-color=\'#000000\'/g" {} +
+        cp -a themes/* $out
+        runHook postInstall
+      '';
+    };
+  nohupXdgOpen = pkgs.writeShellScriptBin "nohup-xdg-open" ''
+    ${pkgs.coreutils}/bin/nohup ${pkgs.xdg-utils}/bin/xdg-open "$@" >/dev/null 2>&1 &
+  '';
+  obsidian =
+    let
+      obsidianHook = pkgs.writeTextFile {
+        name = "obsidian-hook";
+        text = builtins.readFile ./resources/obsidian-hook;
+        executable = true;
+      };
+    in
+    (pkgs.obsidian.overrideAttrs (x: {
+      postInstall = ''
+        sed -i '1 a ${obsidianHook}' $out/bin/obsidian
+      '';
+    }));
+}
