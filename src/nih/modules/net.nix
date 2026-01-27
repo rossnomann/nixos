568e0dc4260d79fd8694dc1229cb61abe8898d4a
Ross Nomann <rossnomann@protonmail.com>
2026-01-27 21:56:57 +0100

    Remove nameservers

--- src/nih/modules/net.nix
+++ src/nih/modules/net.nix
@@ -9,12 +9,6 @@ in
   config = lib.mkIf cfg.enable {
     networking = {
       hostName = cfgNet.host;
-      nameservers = [
-        "8.8.8.8"
-        "8.8.4.4"
-        "1.1.1.1"
-        "1.0.0.1"
-      ];
       networkmanager.enable = true;
       useDHCP = lib.mkDefault true;
     };
