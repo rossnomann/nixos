ab769aee7a803e5f3c5f7935dca88428263b8f6d
Ross Nomann <rossnomann@protonmail.com>
2026-02-15 08:06:25 +0100

    deadbeef build finally fixed

--- src/nih/packages/default.nix
+++ src/nih/packages/default.nix
@@ -4,10 +4,6 @@ _: {
     inputs.fretboard.overlays.${system}.default
     (_: prev: {
       inherit (inputs.darkly.packages.${system}) darkly-qt5 darkly-qt6;
-      # TODO: remove when https://github.com/NixOS/nixpkgs/issues/461474 is resolved
-      # See also https://github.com/NixOS/nixpkgs/issues/466710
-      # Don't forget to remove the input as well
-      inherit (inputs.nixpkgs20251111.legacyPackages.${system}) deadbeef;
       nih = {
         wallpapers = import ./wallpapers { inherit prev; };
       };
