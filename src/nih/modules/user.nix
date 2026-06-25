ae376d50271de90af36395c7f44380b405df50fd
Ross Nomann <rossnomann@protonmail.com>
2026-06-25 05:08:03 +0200

    nushell: install git-completions

--- src/nih/modules/user.nix
+++ src/nih/modules/user.nix
@@ -78,6 +78,9 @@ in
         }
       '';
       ".config/nushell/env.nu".text = ''
+
+        use ${pkgs.nu_scripts}/share/nu_scripts/custom-completions/git/git-completions.nu *
+
         def create_left_prompt [] {
             let path_color = (if (is-admin) { ansi red_bold } else { ansi green_bold })
             let path_dir = match (do { $env.PWD | path relative-to $nu.home-dir }) {
